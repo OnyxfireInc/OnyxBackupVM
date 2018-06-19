48a852c4d592c8fb93c590dbf1b51b6104006793
Lance Fogle <lancefogle@users.noreply.github.com>
2018-06-18 22:37:04 -0400

    Prepare for release

--- onyxbackup-vm.py
+++ onyxbackup-vm.py
@@ -135,7 +135,7 @@ class Cli(object):
 		self.logger.info('  {}: {}'.format(type, str))
 
 	def _setup(self):
-		version = '1.1.0'
+		version = '1.1.1'
 		current_year = datetime.datetime.now().year
 		copyright = 'Copyright (C) {}  OnyxFire, Inc. <https://onyxfireinc.com>'.format(current_year)
 		program_title = 'OnyxBackupVM {}'.format(version)
